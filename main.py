1de6f9b964944e9aab04a7b30950d38169199b17
Zergloin <alexsey.hod@mail.ru>
2024-11-13 23:28:06 +0300

    feat: add Clinic class with methods (add, get, update, remove) for Patient, Insurance, Doctor, Staff, Appointment, Department, Bill.

--- main.py
+++ main.py
@@ -159,5 +159,224 @@ class TreatmentPlan:
         self.treatment_steps = treatment_steps
 
 
+class Clinic:
+    def __init__(self) -> None:
+        self.patients: List[Patient] = []
+        self.doctors: List[Doctor] = []
+        self.staff: List[Staff] = []
+        self.appointments: List[Appointment] = []
+        self.departments: List[Department] = []
+        self.bills: List[Bill] = []
+        self.insurances: List[Insurance] = []
+
+    def add_patient(self, patient: Patient) -> None:
+        self.patients.append(patient)
+
+    def get_patient(self, name: str) -> Patient:
+        try:
+            for patient in self.patients:
+                if patient.name == name:
+                    return patient
+            raise CustomError("Ошибка: Пациент не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def update_patient(self, name: str, updated_patient: Patient) -> None:
+        try:
+            for index, patient in enumerate(self.patients):
+                if patient.name == name:
+                    self.patients[index] = updated_patient
+                    return
+            raise CustomError("Ошибка: Пациент не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def remove_patient(self, patient_name: str) -> None:
+        self._remove_item(self.patients, patient_name)
+
+    def add_insurance(self, insurance: Insurance) -> None:
+        self.insurances.append(insurance)
+
+    def get_insurance(self, policy_number: str) -> Insurance:
+        try:
+            for insurance in self.insurances:
+                if insurance.policy_number == policy_number:
+                    return insurance
+            raise CustomError("Ошибка: Страховка не найдена.")
+        except CustomError as ex:
+            print(ex)
+
+    def update_insurance(self, policy_number: str, updated_insurance: Insurance) -> None:
+        try:
+            for index, insurance in enumerate(self.insurances):
+                if insurance.policy_number == policy_number:
+                    self.insurances[index] = updated_insurance
+                    return
+            raise CustomError("Ошибка: Страховка не найдена.")
+        except CustomError as ex:
+            print(ex)
+
+    def remove_insurance(self, policy_number: str) -> None:
+        try:
+            for index, insurance in enumerate(self.insurances):
+                if insurance.policy_number == policy_number:
+                    del self.insurances[index]
+                    return
+            raise CustomError("Ошибка: Страховка не найдена.")
+        except CustomError as ex:
+            print(ex)
+
+    def add_doctor(self, doctor: Doctor) -> None:
+        self.doctors.append(doctor)
+
+    def get_doctor(self, name: str) -> Doctor:
+        try:
+            for doctor in self.doctors:
+                if doctor.name == name:
+                    return doctor
+            raise CustomError("Ошибка: Врач не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def update_doctor(self, name: str, updated_doctor: Doctor) -> None:
+        try:
+            for index, doctor in enumerate(self.doctors):
+                if doctor.name == name:
+                    self.doctors[index] = updated_doctor
+                    return
+            raise CustomError("Ошибка: Врач не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def remove_doctor(self, doctor_name: str) -> None:
+        self._remove_item(self.doctors, doctor_name)
+
+    def add_staff(self, staff_member: Staff) -> None:
+        self.staff.append(staff_member)
+
+    def get_staff(self, name: str) -> Staff:
+        try:
+            for staff_member in self.staff:
+                if staff_member.name == name:
+                    return staff_member
+            raise CustomError("Ошибка: Сотрудник не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def update_staff(self, name: str, updated_staff: Staff) -> None:
+        try:
+            for index, staff_member in enumerate(self.staff):
+                if staff_member.name == name:
+                    self.staff[index] = updated_staff
+                    return
+            raise CustomError("Ошибка: Сотрудник не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def remove_staff(self, staff_name: str) -> None:
+        self._remove_item(self.staff, staff_name)
+
+    def add_appointment(self, appointment: Appointment) -> None:
+        self.appointments.append(appointment)
+
+    def get_appointment(self, patient_name: str, doctor_name: str) -> Appointment:
+        try:
+            for appointment in self.appointments:
+                if appointment.patient.name == patient_name and appointment.doctor.name == doctor_name:
+                    return appointment
+            raise CustomError("Ошибка: Назначение не найдено.")
+        except CustomError as ex:
+            print(ex)
+
+    def update_appointment(self, patient_name: str, doctor_name: str, updated_appointment: Appointment) -> None:
+        try:
+            for index, appointment in enumerate(self.appointments):
+                if appointment.patient.name == patient_name and appointment.doctor.name == doctor_name:
+                    self.appointments[index] = updated_appointment
+                    return
+            raise CustomError("Ошибка: Назначение не найдено.")
+        except CustomError as ex:
+            print(ex)
+
+    def remove_appointment(self, patient_name: str, doctor_name: str) -> None:
+        try:
+            for index, appointment in enumerate(self.appointments):
+                if appointment.patient.name == patient_name and appointment.doctor.name == doctor_name:
+                    del self.appointments[index]
+                    return
+            raise CustomError("Ошибка: Назначение не найдено.")
+        except CustomError as ex:
+            print(ex)
+
+    def add_department(self, department: Department) -> None:
+        self.departments.append(department)
+
+    def get_department(self, name: str) -> Department:
+        try:
+            for department in self.departments:
+                if department.name == name:
+                    return department
+            raise CustomError("Ошибка: Отдел не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def update_department(self, name: str, updated_department: Department) -> None:
+        try:
+            for index, department in enumerate(self.departments):
+                if department.name == name:
+                    self.departments[index] = updated_department
+                    return
+            raise CustomError("Ошибка: Отдел не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def remove_department(self, department_name: str) -> None:
+        try:
+            for index, department in enumerate(self.departments):
+                if department.name == department_name:
+                    del self.departments[index]
+                    return
+            raise CustomError("Ошибка: Отдел не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def create_bill(self, patient_name: str, amount: float) -> None:
+        patient = self.get_patient(patient_name)
+        if patient:
+            bill = Bill(patient, amount)
+            self.bills.append(bill)
+
+    def get_bill(self, patient_name: str) -> Bill:
+        try:
+            for bill in self.bills:
+                if bill.patient.name == patient_name:
+                    return bill
+            raise CustomError("Ошибка: Счет не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def update_bill(self, patient_name: str, new_amount: float) -> None:
+        try:
+            for bill in self.bills:
+                if bill.patient.name == patient_name:
+                    bill.amount = new_amount
+                    return
+            raise CustomError("Ошибка: Счет не найден.")
+        except CustomError as ex:
+            print(ex)
+
+    def remove_bill(self, patient_name: str) -> None:
+        self._remove_item(self.bills, patient_name, is_bill=True)
+
+    def _remove_item(self, item_list: List[Union[Patient, Doctor, Staff, Bill]], name: str,
+                     is_bill: bool = False) -> bool:
+        for item in item_list:
+            if (is_bill and item.patient.name == name) or (not is_bill and item.name == name):
+                item_list.remove(item)
+                return True
+        print("Ошибка: Не найдено.")
+        return False
+
+
 if __name__ == "__main__":
     pass
