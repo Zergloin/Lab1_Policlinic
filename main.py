42f4d3e45bcdd443f82e7e9283dcf214dc2b19be
Zergloin <alexsey.hod@mail.ru>
2024-11-13 20:46:37 +0300

    feat: add Person, Insurance, MedicalRecord, Prescription classes

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
+from typing import List, Union
+
+
+class Person:
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+
+class Insurance:
+    def __init__(self, provider: str, policy_number: str) -> None:
+        self.provider = provider
+        self.policy_number = policy_number
+
+    def __str__(self) -> str:
+        return f"Insurance(Provider: {self.provider}, Policy_number: {self.policy_number})"
+
+
+class MedicalRecord:
+    def __init__(self, diagnosis: str, treatment: str) -> None:
+        self.diagnosis = diagnosis
+        self.treatment = treatment
+
+
+class Prescription:
+    def __init__(self, medication: str) -> None:
+        self.medication = medication
+
+
 if __name__ == "__main__":
     pass
